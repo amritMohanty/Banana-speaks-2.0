3fac3cabc60980734cf6ef9fef048f4606de556c
Amrit Kumar Mohanty <amritmohanty60@gmail.com>
2023-07-25 14:18:22 +0530

    Made some small changes

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 
 var btnTranslate = document.querySelector("#btn-Translate")
 
-var inputText = document.querySelector("#text--input").innerText
+var inputText = document.querySelector("#text--input")
 
-function eventHandler() {
+var outputText = document.querySelector("#text--output")
+
+var eventHandler = function eventHandler() {
 
     console.log("CLICKED")
-    console.log("input: ",inputText)
+    console.log("input: ",inputText.value)
+    outputText.innerHTML = inputText.value
+    console.log("output: ",outputText.value)
 }
 
-btnTranslate.addEventListener("click",eventHandler());
+btnTranslate.addEventListener("click",eventHandler);
 
 // the input text is to be assigned to a variable
 // initiate a var whose text is shown in the output area
--- README.md
+++ README.md
@@ -8,10 +8,10 @@
     -   showing output in the browser
     -   Making network calls to get relevant date
     -   Error handling network calls 
-### Steps i took:-
+### Steps i took
     -   Installed Postman for testing API call
 
-### Basic steps:-
+### Basic steps
     1. Setting up Index.html and commit
     2. Two ways of putting in script tag
     3. Alert(out) and Prompt(inp) for inp/out
@@ -20,7 +20,7 @@
 
 
 
-### Sidenotes:-
+### Sidenotes
     - since the javascript get interpreted and compiled top to bottom, we should put script file just before ending the body tag.
     - after `33.30` he explains how to get reference for btn-translate in JS file.
         - here querySelector is used and assigned to a variable
